f919790206b9db412dc992077b413bf103740222
MariusRask <mara1010@stud.ek.dk>
2026-02-19 11:25:04 +0100

    committ

--- src/UltimateLoanTracker/Book.java
+++ src/UltimateLoanTracker/Book.java
@@ -1,4 +1,26 @@
 package UltimateLoanTracker;
 
-public class Book {
+public class Book extends Product {
+
+    private String author;
+
+    private static final int loanDays = 7;
+    private static final double lateFee = 1.5;
+
+    // Constructor
+    public Book(String title) {
+        this(title, "John Doe");
+    }
+
+    public Book(String title, String author) {
+        super(title, loanDays, lateFee);
+        this.author = author;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book\t" + super.toString() + "\tAuthor:\t" + author;
+    }
 }
+
