6b9f394e65d23a409ec05c8ce4dc4ba57e1ad8f3
SebastianH80 <sehi1000@stud.ek.dk>
2026-02-19 09:45:38 +0100

    Konstruktør upd.

--- src/UltimateLoanTracker/Product.java
+++ src/UltimateLoanTracker/Product.java
@@ -1,4 +1,22 @@
 package UltimateLoanTracker;
 
-public class Product {
+public abstract class Product {
+    // Attributer
+    private String title;
+    private int loanDays;
+    private double lateFee;
+
+
+    // Konstruktør
+    public Product(String title, int loanDays, double lateFee) {
+        this.title = title;
+        this.loanDays = loanDays;
+        this.lateFee = lateFee;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
 }
