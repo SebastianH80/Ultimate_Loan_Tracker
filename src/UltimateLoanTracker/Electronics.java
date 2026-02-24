41f364715799dd4c1cb67329e81cdeb010c84ecb
ChefenML <milo1000@stud.ek.dk>
2026-02-24 11:16:24 +0100

    electronis update, no new lines

--- src/UltimateLoanTracker/Electronics.java
+++ src/UltimateLoanTracker/Electronics.java
@@ -17,14 +17,14 @@ public class Electronics extends Product{
     //Polymorph metode
     @Override
     public void useProduct() {
-        //do something();
+        System.out.println("Gruppe 7 leger med " + this.model);
     }
     //override toString so everything prints
     @Override
     public String toString() {
         return "Electronic \t" + super.toString() +
-                "\nModel: \t" + this.model +
-                "\nKitlevel\t: " + this.kitLevel +
-                "\nBasevalue\t: " + this.baseValue;
+                "Model: \t" + this.model +
+                "Kitlevel\t: " + this.kitLevel +
+                "Basevalue\t: " + this.baseValue;
     }
 }
